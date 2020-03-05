631a60cb053933248e2edbef181c17a2ecbd3492
Shivam Shrirao <shivamshrirao@gmail.com>
2020-03-05 15:05:04 +0530

    project initialized

---
+++ src/main/java/com/example/gradSpingBoot/GradSpingBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradSpingBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradSpingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradSpingBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradSpingBoot/GradSpingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradSpingBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradSpingBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
