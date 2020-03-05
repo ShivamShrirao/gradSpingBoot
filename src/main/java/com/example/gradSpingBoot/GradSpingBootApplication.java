2e53dd32fe53db4fc358ae2215102102a21a7f1f
Shivam Shrirao <shivamshrirao@gmail.com>
2020-03-05 16:18:36 +0530

    tomcat running war

--- src/main/java/com/example/gradSpingBoot/GradSpingBootApplication.java
+++ src/main/java/com/example/gradSpingBoot/GradSpingBootApplication.java
@@ -2,12 +2,15 @@ package com.example.gradSpingBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GradSpingBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GradSpingBootApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(GradSpingBootApplication.class, args);
+		Alien a = ctx.getBean(Alien.class);
+		a.show();
 	}
 
 }
