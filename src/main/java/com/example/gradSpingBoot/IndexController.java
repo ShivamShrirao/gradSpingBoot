5a6db4ec6cad1f640c7901d2359e321bdee5ff12
Shivam Shrirao <shivamshrirao@gmail.com>
2020-03-06 10:21:57 +0530

    basic web page

---
+++ src/main/java/com/example/gradSpingBoot/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.gradSpingBoot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("index")
+    public String index(){
+        System.out.println("hhhhh");
+        return "index";
+    }
+}
